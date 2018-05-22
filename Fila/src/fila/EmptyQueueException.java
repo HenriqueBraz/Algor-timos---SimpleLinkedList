7469fd671e18463b829100e82eeda0206da41107
HenriqueBraz <38267870+HenriqueBraz@users.noreply.github.com>
2018-05-21 22:36:48 -0300

    Add files via upload

---
+++ Fila/src/fila/EmptyQueueException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fila;
+
+/**
+ *
+ * @author 17120069
+ */
+public class EmptyQueueException extends RuntimeException
+{
+public EmptyQueueException(String s) { super(s); }
+}
